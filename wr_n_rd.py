1fc7ead73e4f74deff40bb88c6430e05fc20b23f
radugaboost <113132811+radugaboost@users.noreply.github.com>
2022-12-19 15:01:25 +0300

    Job done, back to work

---
+++ wr_n_rd.py
@@ -0,0 +1,76 @@
+"""Readers and writers."""
+from threading import Thread, Condition, Event
+from time import sleep
+from random import randint
+
+
+TEXT = ['Aboba', 'Kokos', 'Kakat']
+current_letter = ''
+
+
+class Reader(Thread):
+    """Class of reader."""
+
+    def __init__(self, name):
+        """Initialization of reader.
+
+        Args:
+            name (str): name of reader.
+        """
+        super().__init__(name=name)
+        self.name = name
+
+    def run(self):
+        """Run method of reader."""
+        global event
+        while event.wait():
+            print('Reader {0} read letter {1}'.format(self.name, current_letter))
+
+
+class Writer(Thread):
+    """Class of writer."""
+
+    TIMEOUT_WRITING = (3, 6)
+    TIME_OF_SLEEP = (2, 4)
+
+    def __init__(self, name):
+        """Inialization of writer.
+
+        Args:
+            name (str): name of writer.
+        """
+        super().__init__(name=name)
+        self.name = name
+
+    def run(self):
+        """Run method of writer."""
+        global cond
+        global event
+        global current_letter
+        with cond:
+            cond.wait()
+            text = TEXT[randint(0, len(TEXT) - 1)]
+            print('Writer {0} starting writing'.format(self.name))
+            res = text
+            while text:
+                letter, text = text[:1], text[1:]
+                event.set()
+                current_letter = letter
+                print('Writer {0} wrote {1}'.format(self.name, letter))
+                event.clear()
+                sleep(randint(*Writer.TIMEOUT_WRITING))
+            print('Writer {0} has wrote {1}'.format(self.name, res))
+            print('Writer {0} goes to sleep'.format(self.name))
+            sleep(randint(*Writer.TIME_OF_SLEEP))
+
+
+if __name__ == "__main__":
+    cond = Condition()
+    event = Event()
+    for nums in range(10):
+        Reader(str(nums)).start()
+    while True:
+        for num in range(2):
+            Writer(str(num)).start()
+        with cond:
+            cond.notify_all()
